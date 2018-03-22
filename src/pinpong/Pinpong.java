37675170525423ddde4349d154fad5521a9d631f
Marcos <Marcos@192.168.1.5>
2018-03-22 05:12:04 +0100

    1

---
+++ src/pinpong/Pinpong.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pinpong;
+
+import pinpong.Ventana;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HOME
+ */
+public class Pinpong {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+
+    }
+}
