aab793d421422e1c34a8fe4b6f02163ad92635f7
Marcos <Marcos@192.168.1.5>
2018-03-22 05:12:11 +0100

    2

---
+++ src/pinpong/Ventana.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pinpong;
+import javax.swing.JFrame;
+public class Ventana extends JFrame {
+
+    private final int ANCHO = 800, ALTO = 500;
+    private TableroJuego lamina;
+    private Hilo hilo;
+    public Ventana() {
+        setTitle("Pong");
+        setSize(ANCHO, ALTO);
+        setLocationRelativeTo(null); //ubicando la ventana en el centro de la pantalla
+        setResizable(false);
+        lamina = new TableroJuego();
+        add(lamina);
+        addKeyListener(new EventoTeclado());
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        hilo=new Hilo(lamina);
+        hilo.start();
+        
+    }
+}
